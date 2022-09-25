db96efca35f78c74315d79e7607225ba9a896bc4
Roy A Vanegas <roy@thecodeeducators.com>
2022-09-25 13:35:52 -0400

    Add Microsoft’s Makefile tools extension ID for VS Code

--- includes/globals/variables.sh
+++ includes/globals/variables.sh
@@ -38,7 +38,7 @@ FULL_NAME="$(finger "$USER_NAME" | awk '/Name:/ {print $4" "$5}')"
 # name was retrieved above.
 USER_SHELL="$(finger "$USER_NAME" | awk '/Shell:/ {print $4}')"
 
-VSCODE_EXTENSIONS=("editorconfig.editorconfig" "shengchen.vscode-checkstyle" "vscjava.vscode-java-pack" "VisualStudioExptTeam.vscodeintellicode" "redhat.java")
+VSCODE_EXTENSIONS=("editorconfig.editorconfig" "shengchen.vscode-checkstyle" "vscjava.vscode-java-pack" "VisualStudioExptTeam.vscodeintellicode" "redhat.java" "ms-vscode.makefile-tools")
 
 export VSCODE_EXTENSIONS
 export ATOM_PACKAGES
