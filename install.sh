0f7bf592d134f5c390f09fa7700a647e2a1df1e6
Roy A Vanegas <roy@thecodeeducators.com>
2023-01-02 04:55:36 -0500

    Add v19 of Java on macOS M1 chip

--- install.sh
+++ install.sh
@@ -169,6 +169,21 @@ install_typeface \
 print_msg "log" "Installing Java..."
 echo ""
 
+install 'Java 19 SDK (Apple M1 Chip)' 'jdk-19_macos-aarch64_bin.dmg' 'https://download.oracle.com/java/19/latest/jdk-19_macos-aarch64_bin.dmg' \
+-H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:108.0) Gecko/20100101 Firefox/108.0' \
+-H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' \
+-H 'Accept-Language: en-US,en;q=0.5' \
+-H 'Accept-Encoding: gzip, deflate, br' \
+-H 'Referer: https://www.oracle.com/java/technologies/downloads/' \
+-H 'DNT: 1' \
+-H 'Connection: keep-alive' \
+-H 'Upgrade-Insecure-Requests: 1' \
+-H 'Sec-Fetch-Dest: document' \
+-H 'Sec-Fetch-Mode: navigate' \
+-H 'Sec-Fetch-Site: same-site' \
+-H 'Sec-Fetch-User: ?1' \
+-H 'Sec-GPC: 1'
+
 install 'Java 19 SDK (Intel Chip)' 'jdk-19_macos-x64_bin.dmg' 'https://download.oracle.com/java/19/latest/jdk-19_macos-x64_bin.dmg' \
 -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:108.0) Gecko/20100101 Firefox/108.0' \
 -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8' \
